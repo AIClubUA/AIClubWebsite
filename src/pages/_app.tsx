320a02072973b62b097cccb8d5dbb3ebd4437214
Connor Barbaccia <connorbarbaccia@gmail.com>
2025-03-19 18:23:31 -0500

    updated side bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,17 @@ import Sidebar from '@/components/SidebarComp';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="flex h-screen">
-      {/* Sidebar */}
-      <Sidebar />
+    <div className="flex">
+      {/* Fixed Sidebar */}
+      <div className="fixed left-0 top-0 h-screen w-64 bg-black text-white">
+        <Sidebar />
+      </div>
 
-      {/* Main Content */}
-      <div className="flex-1">
-        <Component {...pageProps} />
+      {/* Main Content Wrapper */}
+      <div className="flex-1 min-h-screen ml-64">
+        <div className="max-w-4xl mx-auto p-8">
+          <Component {...pageProps} />
+        </div>
       </div>
     </div>
   );
